e3f8852cbe0a4a24d472f9f615068be02326fac5
KizeRemi <remi.mavillaz@live.fr>
2017-09-19 14:42:39 +0200

    test

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,39 @@
+<?php
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\Controller\Annotations as FOSRest;
+
+use UserBundle\Event\RegistrationEvent;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+class DefaultController extends Controller implements ClassResourceInterface
+{
+    /**
+     * @ApiDoc(
+     *  section="Tests",
+     *  description="Get a test",
+     *  resource = true,
+     *  statusCodes = {
+     *     200 = "Successful",
+     *     404 = "Not found"
+     *   }
+     * )
+     * @FOSRest\Get("/")
+     */
+    public function getAction()
+    {
+        return [];
+    }
+}
